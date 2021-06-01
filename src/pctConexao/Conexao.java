70f082d07aa2d45f8df51c7fe7a63fae17b18f6d
João Lucas de Oliveira Lopes <jlucaso@hotmail.com>
2021-05-31 21:18:55 -0300

    Nova conexão com o banco e resolvido mensagem SSL

--- src/pctConexao/Conexao.java
+++ src/pctConexao/Conexao.java
@@ -9,7 +9,7 @@ import java.sql.SQLException;
 public class Conexao {
     public Connection getConnection() {
         try {
-            return DriverManager.getConnection("jdbc:mysql://localhost/animaview", "root", "root");
+            return DriverManager.getConnection("jdbc:mysql://remotemysql.com/0TPmdlvjbi?autoReconnect=true&useSSL=false", "0TPmdlvjbi", "MgAO6lP8rK");
         } catch (SQLException erro) {
             throw new RuntimeException(erro);
         }
