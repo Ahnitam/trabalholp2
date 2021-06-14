84e85bf0de4a354998ec9a5fd6538a24175c72ab
Higor Pires <higorpiresdefranca60@gmail.com>
2021-06-14 20:48:43 -0300

    Funçao cadastrar animes funcionando

--- src/animaview/Animaview.java
+++ src/animaview/Animaview.java
@@ -1,13 +1,13 @@
 package animaview;
+
+import pctDAO.AnimeDAO;
+
 /**
  *
  * @author Luis Henrique
  */
 public class Animaview {
-
- 
     public static void main(String[] args) {
-       
     }
     
 }
